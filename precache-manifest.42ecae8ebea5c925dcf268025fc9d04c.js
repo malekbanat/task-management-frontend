cf7d06cd192bcaf568e5b7230ec076ff5f8a8c50
malekbanat <malekbanat@users.noreply.github.com>
2021-12-30 10:57:27 +0000

    deploy: 86fcba3e3bb552e73d1f2358f48ceabe028361b5

---
+++ precache-manifest.42ecae8ebea5c925dcf268025fc9d04c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0963f5ae6d11fdccc524b75f1b8c6d12",
+    "url": "https://malekbanat.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "3c63d3e169c358ea7d24",
+    "url": "https://malekbanat.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "e504197521439937e982",
+    "url": "https://malekbanat.github.io/task-management-frontend/static/js/2.edb30e6c.chunk.js"
+  },
+  {
+    "revision": "3c63d3e169c358ea7d24",
+    "url": "https://malekbanat.github.io/task-management-frontend/static/js/main.80f0066a.chunk.js"
+  },
+  {
+    "revision": "651d73e8e1a33b0231c1",
+    "url": "https://malekbanat.github.io/task-management-frontend/static/js/runtime~main.065546c8.js"
+  }
+]);
